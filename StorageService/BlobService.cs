f610d85ee1202473ae9b8dd02cfc04a908f7a718
gustavomirandag <gustavomirandag@gmail.com>
2018-08-07 19:45:01 -0300

    ImageGallery using Microsoft Azure Storage Account

---
+++ StorageService/BlobService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageService
+{
+    public class BlobService
+    {
+        private CloudStorageAccount _cloudStorageAccount;
+
+        public BlobService()
+        {
+            _cloudStorageAccount = CloudStorageAccount.Parse(StorageService.Properties.Settings.Default.StorageConnectionString);
+        }
+
+        /// <summary>
+        /// Retorna o endereço da imagem
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="fileName"></param>
+        /// <param name="inputStream"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public async Task<string> UploadImage(string container, string fileName, System.IO.Stream fileStream, string contentType)
+        {
+            //Classe que faz acesso ao Azure Storage Blob
+            CloudBlobClient blobClient = _cloudStorageAccount.CreateCloudBlobClient();
+
+            //Classe que faz referência a um Container
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference(container);
+
+            //Cria um container novo se não existe
+            await blobContainer.CreateIfNotExistsAsync();
+
+            //Altera a configuração do container para permitir o acesso anônimo
+            await blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            //Referência a uma imagem
+            CloudBlockBlob cloudBlockBlob = blobContainer.GetBlockBlobReference(fileName);
+            cloudBlockBlob.Properties.ContentType = contentType;
+
+            //Upload não assíncrono
+            Task task = cloudBlockBlob.UploadFromStreamAsync(fileStream);
+
+            //Blob URL
+            return cloudBlockBlob.Uri.ToString();
+        }
+
+
+    }
+}
---
+++ WebImageGallery/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebImageGallery.Models;
+using StorageService;
+using System.Threading.Tasks;
+
+namespace WebImageGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Image> _images;
+
+        public HomeController()
+        {
+            if (_images == null)
+                _images = new List<Image>();
+        }
+
+        public ActionResult Index()
+        {
+            return View(_images);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UploadImage(string description, HttpPostedFileBase imageFile)
+        {
+            Image image = new Image();
+            image.Description = description;
+            var blobService = new BlobService();
+            image.Url = await blobService.UploadImage("gallerycontainer",
+                imageFile.FileName,
+                imageFile.InputStream,
+                imageFile.ContentType);
+
+            _images.Add(image);
+
+            return View("Index", _images);
+        }
+    }
+}
